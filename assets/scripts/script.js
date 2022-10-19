69c9b63ee84c4ffc659b99ac19955cceaf2537b2
Mikael Josias <mikaeljosias2002@gmail.com>
2022-10-18 23:48:47 -0300

    feat(script): create a script that asks the number of cards

---
+++ assets/scripts/script.js
@@ -0,0 +1,15 @@
+
+let promptMsg = "Digite o número de cartas com que gostaria de jogar:";
+let numCards = 0;
+
+while(numCards === 0){
+    let num = Number(prompt(promptMsg));
+
+    if (num%2 === 0 && 4 <= num && num <= 14) {
+
+        numCards = num;        
+    }
+
+    promptMsg = "O número de cartas deve ser par, entre 4 e 14:"
+}
+
